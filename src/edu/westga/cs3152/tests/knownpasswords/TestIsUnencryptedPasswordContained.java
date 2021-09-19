c6bda56b21ec3506f81452790ce12adb8fa4a70a
Alex <contact@alexdecesare.com>
2021-09-19 11:41:03 -0400

    Removed non static import
    Removed a non static import

--- src/edu/westga/cs3152/tests/knownpasswords/TestIsUnencryptedPasswordContained.java
+++ src/edu/westga/cs3152/tests/knownpasswords/TestIsUnencryptedPasswordContained.java
@@ -3,7 +3,6 @@ package edu.westga.cs3152.tests.knownpasswords;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
